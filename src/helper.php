5e373bf05153c97dce3d9a4f9f134df0c9418f72
wenbo <wenbo@wenber.club>
2021-12-30 00:01:46 +0800

    增加简单辅助函数 cache

--- src/helper.php
+++ src/helper.php
@@ -1,11 +1,18 @@
 <?php
 
 use Hyperf\Utils\ApplicationContext;
+use WilburYu\HyperfCacheExt\CacheInterface;
 use WilburYu\HyperfCacheExt\CounterLimiter;
 
 if (!function_exists('rate_limiter')) {
     function counter_limiter()
     {
         return ApplicationContext::getContainer()->get(CounterLimiter::class);
+    }
+}
+if (!function_exists('cache')) {
+    function cache(): CacheInterface
+    {
+        return ApplicationContext::getContainer()->get(CacheInterface::class);
     }
 }
