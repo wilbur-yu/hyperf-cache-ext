6f459668cbf21b0b6527483dff4d6229e726e0cf
wenbo <wenbo@wenber.club>
2021-12-30 11:28:40 +0800

    增加 CounterRateLimitWithRedis 注解

---
+++ src/Annotation/CounterRateLimitWithRedis.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of project wilbur-yu/hyperf-cache-ext.
+ *
+ * @author   wenbo@wenber.club
+ * @link     https://github.com/wilbur-yu
+ */
+
+namespace WilburYu\HyperfCacheExt\Annotation;
+
+use Attribute;
+use Hyperf\Di\Annotation\AbstractAnnotation;
+
+/**
+ * @Annotation
+ * @Target({"CLASS", "METHOD"})
+ */
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+class CounterRateLimitWithRedis extends CounterRateLimit
+{
+    /**
+     * @var string|callable
+     */
+    public $key;
+    /**
+     * @var int|string
+     */
+    public $maxAttempts;
+    /**
+     * @var int
+     */
+    public $decayMinutes;
+    /**
+     * @var string
+     */
+    public $named;
+    /**
+     * @var string
+     */
+    public $prefix;
+}
---
+++ src/Aspect/CounterRateLimitWithRedisAnnotationAspect.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of project wilbur-yu/hyperf-cache-ext.
+ *
+ * @author   wenbo@wenber.club
+ * @link     https://github.com/wilbur-yu
+ */
+
+namespace WilburYu\HyperfCacheExt\Aspect;
+
+use WilburYu\HyperfCacheExt\Annotation\CounterRateLimitWithRedis;
+use WilburYu\HyperfCacheExt\Redis\Limiters\DurationLimiter;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Redis\Redis;
+use Psr\Http\Message\ResponseInterface;
+use Hyperf\Utils\Context;
+
+#[Aspect]
+class CounterRateLimitWithRedisAnnotationAspect extends CounterRateLimitAnnotationAspect
+{
+    public $annotations = [
+        CounterRateLimitWithRedis::class,
+    ];
+
+    /**
+     * The timestamp of the end of the current duration by key.
+     */
+    public array $decaysAt = [];
+
+    /**
+     * The number of remaining slots by key.
+     */
+    public array $remaining = [];
+
+    /**
+     * @param  array                               $limits
+     * @param  \Hyperf\Di\Aop\ProceedingJoinPoint  $proceedingJoinPoint
+     *
+     * @throws \Hyperf\Di\Exception\Exception
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected function handleRequest(array $limits, ProceedingJoinPoint $proceedingJoinPoint): ResponseInterface
+    {
+        foreach ($limits as $limit) {
+            if ($this->tooManyAttempts($limit->key, $limit->maxAttempts, $limit->decayMinutes)) {
+                throw $this->buildException($limit->key, $limit->maxAttempts);
+            }
+        }
+
+        $response = $proceedingJoinPoint->process();
+        Context::set(ResponseInterface::class, $response);
+
+        foreach ($limits as $limit) {
+            $response = $this->addHeaders(
+                $response,
+                $limit->maxAttempts,
+                $this->calculateRemainingAttempts($limit->key, $limit->maxAttempts)
+            );
+        }
+
+        return $response;
+    }
+
+    /**
+     * Calculate the number of remaining attempts.
+     *
+     * @param  string    $key
+     * @param  int       $maxAttempts
+     * @param  int|null  $retryAfter
+     *
+     * @return int
+     */
+    protected function calculateRemainingAttempts(string $key, int $maxAttempts, int $retryAfter = null): int
+    {
+        return is_null($retryAfter) ? $this->remaining[$key] : 0;
+    }
+
+    /**
+     * Get the number of seconds until the lock is released.
+     *
+     * @param  string  $key
+     *
+     * @return int
+     */
+    protected function getTimeUntilNextRetry(string $key): int
+    {
+        return $this->decaysAt[$key] - $this->currentTime();
+    }
+
+    /**
+     * Determine if the given key has been "accessed" too many times.
+     *
+     * @param  string  $key
+     * @param  int     $maxAttempts
+     * @param  int     $decayMinutes
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @return mixed
+     */
+    protected function tooManyAttempts(string $key, int $maxAttempts, int $decayMinutes): mixed
+    {
+        $limiter = new DurationLimiter(
+            $this->container->get(Redis::class),
+            $key,
+            $maxAttempts,
+            $decayMinutes * 60
+        );
+
+        return tap(!$limiter->acquire(), function () use ($key, $limiter) {
+            [$this->decaysAt[$key], $this->remaining[$key]] = [
+                $limiter->decaysAt,
+                $limiter->remaining,
+            ];
+        });
+    }
+
+    protected function parseConfig(ConfigInterface $config)
+    {
+        $limiterConfig = parent::parseConfig($config);
+        $limiterConfig['prefix'] = $config->get('cache.default.prefix').$limiterConfig['prefix'];
+
+        return $limiterConfig;
+    }
+}
